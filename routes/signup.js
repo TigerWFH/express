5804ed9973af0c93c70a8ac9a4346b8a0b8a3d31
monkey-office <monkey@lwork.com>
2017-02-17 19:12:40 +0800

    commit

---
+++ routes/signup.js
@@ -0,0 +1,15 @@
+let express = require('express');
+let router = express.Router();
+let User = require('../models/users');
+let users = new User();
+
+// 注册新用户
+router.post('/', (req, res, next) => {
+    console.log('name--->', req.body.name);
+    res.json({
+        msgCode: 0,
+        msgText: 'success'
+    });
+});
+
+module.exports = router;
